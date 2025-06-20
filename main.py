dc08968ff932720bcf7efe324322c7247c81f20f
Ayan <ayanyadav2619@gmail.com>
2025-06-20 15:29:01 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from kidney_disease_classification import logger
 from kidney_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from kidney_disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
-STAGE_NAME = "Data Ingestion stage" 
-
-if __name__ == "__main__":
-    try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-
-    except Exception as e:
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
         logger.exception(e)
         raise e
-    
-    
+
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
